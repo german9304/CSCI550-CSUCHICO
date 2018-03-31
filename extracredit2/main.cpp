631a6a6db861b6aedde4c455e45573ffbda13c16
german9304 <31298672+german9304@users.noreply.github.com>
2018-03-31 00:48:34 -0700

    project completed

--- extracredit2/main.cpp
+++ extracredit2/main.cpp
@@ -1,8 +1,29 @@
 
 #include <iostream> 
 #include <vector> 
+#include <algorithm>
 using namespace std;
 
+int max_revenue(vector<int> & prices,int length){
+   int size = prices.size();
+     vector<int> c(size+1,0);
+     int max=0;
+     for(unsigned  int i=0;i<prices.size();i++){
+        int min =-1;
+         for(unsigned  int j=0;j<i+1;j++){
+           if(min < prices[j]+c[(i+1)-(j+1)]){
+              min = prices[j]+c[(i+1)-(j+1)];
+            }
+         }
+       c[i+1]=min;
+       if(max<min){
+         max = min;
+       }
+     }
+
+   return max;
+
+}
 int main(){
   int length_square_land;
   cin>>length_square_land;
@@ -12,6 +33,7 @@ int main(){
       cin>>prices;
       current_prices.push_back(prices);
   }
-
+ int total = max_revenue(current_prices,length_square_land);
+ cout << total <<endl;
     return 0;
 }
