4587e415334d5b4101254f26d796cb4a612bbef3
german9304 <31298672+german9304@users.noreply.github.com>
2018-04-17 00:40:24 -0700

    lms

--- p4/proj4.cpp
+++ p4/proj4.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
+void count(vector<int> & count_c,int str_length,string cat,vector<int> & new_intername){
+
+	for(int i=0;i<str_length;i++){
+		int a = cat[i];
+		count_c[a]++;
+	}
+	int rename_index=1;
+  for(int i=0;i<256;i++){
+		if(count_c[i]>0){
+			char a = i;
+			new_intername[i]=rename_index++;
+			cout <<rename_index << " " << a << " " << new_intername[i] << " " <<endl;
+		}
+	}
+  for(int i=0;i<256;i++){
+		if(count_c[i]>0){
+			cout << new_intername[i] <<endl;
+		//	cout <<a << " " << cc[i] << " " <<endl;
+		}
+	}
+}
+void substring_lms(string in){
+	in+="$";
+	cout <<"sring: " << in <<endl;
+}
 int main(){
 	string in;
 	string cat;
@@ -10,17 +36,13 @@ int main(){
 	cat+=in;
 	getline(cin, in);
   }
-	int count[256]={0};
-	for(int i=0;i<cat.length();i++){
-		int a = cat[i];
-		count[a]++;
-	}
-  for(int i=0;i<256;i++){
-		if(count[i]>0){
-	    char a = i;
-			cout <<a << " " << count[i] << " " <<endl;
-		}
-	}
+
+	vector<int> cc(256,0);
+	vector<int> newinteger(256,0);
+	vector<int> lms(256,0);
+	int str_length= cat.length();
+	count(cc,str_length,cat,newinteger);
+
 
 
 	return 0;
