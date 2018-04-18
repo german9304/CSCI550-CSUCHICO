9c7ea517c155620cd55fe91b65afa67c7490b9e0
german9304 <31298672+german9304@users.noreply.github.com>
2018-04-18 13:37:34 -0700

    left right

--- p4/proj4.cpp
+++ p4/proj4.cpp
@@ -17,7 +17,7 @@ vector<int> & occurances, vector<int> & smaller_characters, vector<int> & end_bu
 	for(int i=0;i<256;i++){
 		char a = i;
 			if(arr[i]>0){
-			//	cout  << " " <<a << " " << 	arr[i] <<" "<<endl;
+			cout  << " " <<a << " " << 	arr[i] <<" "<<endl;
 			}
 	}
 	cout <<endl;
@@ -49,9 +49,7 @@ vector<int> & occurances, vector<int> & smaller_characters, vector<int> & end_bu
 	}
 	cout <<endl;
 	//cout <<endl;
-	for(int i=0;i<end_bucket.size();i++){
-			cout <<  end_bucket[i]  << endl;
-	}
+
 	/*
 	int rename_index=1;
   for(int i=0;i<256;i++){
@@ -73,15 +71,12 @@ vector<int> & occurances, vector<int> & smaller_characters, vector<int> & end_bu
 	*/
 
 }
-void substring_lms(string in){
-	in+="$";
-	cout <<"sring: " << in <<endl;
-}
-void type_suffix_array(int arr[],vector<char> & type_suffix,string in,
+void type_suffix_array(int arr[],vector<int> & type_suffix,string in,
 	vector<int> & suffix_array,vector<int> & end_bucket){
+  vector<int> copy_end_bucket = end_bucket;
 	int end = in.length()-1;
 	//cout <<"size: " << type_suffix.size() <<endl;
-	type_suffix[end]='s';
+	type_suffix[end]=1;
 	for(int i=end-1;i>=0;i--){
 		/*
 		int  b = in[i+1];
@@ -89,40 +84,40 @@ void type_suffix_array(int arr[],vector<char> & type_suffix,string in,
 		<< " end_bucket " <<end_bucket[arr[b]] <<endl;
 		*/
 		if(in[i]>in[i+1]){
-			type_suffix[i]='l';
+			type_suffix[i]=0;
 			if(in[(i)+1]=='$'){
 				suffix_array[0]=14;
 				end_bucket[0]=-1;
 			}else{
-				if(type_suffix[i+1]=='s'){
+				if(type_suffix[i+1]==1){
 					//suffix_array[0]=14;
 					int  b = in[i+1];
-					int index = end_bucket[arr[b]];
+					int index = copy_end_bucket[arr[b]];
 					// cout << i << " " << i+1 <<endl;
 					suffix_array[index]= i+1;
-					end_bucket[arr[b]]--;
+					copy_end_bucket[arr[b]]--;
 					// cout << i << " " << in[i] << " " << in[i+1] <<" " <<  end_bucket[arr[b]]<<endl;
 				}
 			}
 		}else if(in[i]<in[i+1]){
-			type_suffix[i]='s';
+			type_suffix[i]=1;
 		}else{
-			if(type_suffix[i+1]=='l'){
+			if(type_suffix[i+1]==0){
 				//cout <<"equal" << in[i+1] << " " <<in[i] <<endl;
-				type_suffix[i]='l';
+				type_suffix[i]=0;
 			}else{
-				type_suffix[i]='s';
+				type_suffix[i]=1;
 			}
 		}
 	//	cout <<"(i) " << in[i] << " (i + 1) " << in[i+1] << "  *  ";
 	}
-	/*
+/*
 	cout <<endl;
 	for(int i=end-1;i>=0;i--){
 		cout <<"(i) " << in[i] << " (i + 1) " << in[i+1] << "  *  ";
 	}
 	cout <<endl;
-	*/
+
 
 	cout << in << " "<<endl;
 	for(int i=0;i<type_suffix.size();i++){
@@ -133,9 +128,124 @@ void type_suffix_array(int arr[],vector<char> & type_suffix,string in,
 	for(int i=0;i<suffix_array.size();i++){
 		cout <<suffix_array[i] << " ";
 	}
-
+  */
+  /*
+  cout <<endl;
+	cout <<"copy_end_bucket" <<endl;
+	for(int i=0;i<copy_end_bucket.size();i++){
+		cout <<copy_end_bucket[i] << " ";
+	}
+  */
 	//for(int i=0;i<end_bucket.size())
 	//cout <<endl;
+}
+void induce_sort(int arr[],vector<int> & suffix_array,vector<int> & t_array,
+string in,vector<int> & head_bucket,vector<int> & end_bucket){
+  vector<int> copy_end_bucket = end_bucket;
+  vector<int> copy_head_bucket = head_bucket;
+  cout <<endl;
+	cout << "head_bucket" <<endl;
+    for(int i=0;i<head_bucket.size();i++){
+      cout << head_bucket[i] <<" ";
+    }
+    cout <<endl;
+      for(int i=0;i<t_array.size();i++){
+        cout << t_array[i] <<" ";
+      }
+	/*
+  for(int i=0;i<in.length();i++){
+		char a = in[i];
+		int b = a;
+		cout <<a << " " << b << " " << arr[b] << " head: " <<  head_bucket[arr[b]]<<endl;
+	}
+
+  cout <<endl;
+  cout <<"t_array" <<endl
+  */
+  /*
+  cout <<"t_array" <<endl;
+  for(int i=0;i<t_array.size();i++){
+    cout << t_array[i] <<" ";
+  }
+  */
+  cout <<endl;
+  cout << "chars" <<endl;
+  int size = suffix_array.size();
+  /*
+   Left-to-Right scanning
+  */
+	for(int i=0;i<suffix_array.size();i++){
+  		int p = suffix_array[i];
+      if(p!=-1){
+        if(p==0){
+          //cout <<"0" <<endl;
+          char a = in[size-1];
+          //cout << "a " << a << " "<<endl;
+          if(suffix_array[0]==-1){
+              suffix_array[0]=0;
+          }
+
+        }else{
+        if(!t_array[p-1]){
+
+        //  cout << t_array[p-1] <<" ";
+
+          char a = in[p-1];
+          //cout << a <<" ";
+          int b = a;
+          int index = arr[b];
+          //cout <<p-1 << " " << i << " " << a << " " <<  index <<endl;
+          //if(p==0){
+          int index_head = copy_head_bucket[index];
+          suffix_array[index_head] = p-1;
+          copy_head_bucket[index]++;
+          //break;
+        //  cout <<a <<endl;
+        //  cout << "head: " << copy_head_bucket[index] <<endl;
+        }
+      }
+    }
+  }
+  cout <<endl;
+  cout << "new suffix_array left right" <<endl;
+  for(int i=0;i<suffix_array.size();i++){
+    cout << suffix_array[i] <<" ";
+  }
+  cout <<endl;
+
+  /*
+    Right-to-Left scanning
+  */
+  copy_end_bucket = end_bucket;
+  cout << "right to left " <<endl;
+  int end = suffix_array.size();
+	for(int i=end-1;i>=0;i--){
+  //cout << suffix_array[i] << " ";
+    int p = suffix_array[i];
+    if(p==0){
+
+    }else{
+     if(t_array[p-1]){
+       char a = in[p-1];
+       int b = a;
+      // cout << a <<endl;
+       int index = arr[b];
+       int end_bucket_index= copy_end_bucket[index];
+        //suffix_array[i]
+       //cout << "end: " << copy_end_bucket[index] << " p: " << p-1 << endl;
+       suffix_array[end_bucket_index]=p-1;
+       copy_end_bucket[index]--;
+
+       //break;
+       }
+     }
+    //cout << t_array[p-1] << " ";
+  }
+    cout << "new suffix_array right left" <<endl;
+    for(int i=0;i<suffix_array.size();i++){
+      cout << suffix_array[i] <<" ";
+    }
+  cout <<endl;
 }
 int main(){
 	string in;
@@ -149,15 +259,21 @@ int main(){
   int arr[256]={0};
 	vector<int> newinteger;
 	vector<int> occurances;
-	vector<int> smaller_characters;
+	vector<int> head_bucket;
 	vector<int> end_bucket;
-	vector<char> t_array(str_length+1,'\0');
-	count(arr,str_length,cat,newinteger,occurances,smaller_characters
+	vector<int> t_array(str_length+1,-1);
+	count(arr,str_length,cat,newinteger,occurances,head_bucket
 	,end_bucket);
+  cout << "end_bucket" <<endl;
+  for(int i=0;i<end_bucket.size();i++){
+    cout << end_bucket[i] << " ";
+  }
+  cout <<endl;
 	string str_final = 	cat+="$";
-	vector<int> suffix_array(str_final.length(),0);
+	vector<int> suffix_array(str_final.length(),-1);
   type_suffix_array(arr,t_array,str_final,suffix_array,end_bucket);
-
+	induce_sort(arr,suffix_array,t_array,str_final,head_bucket,end_bucket);
+	cout <<endl;
 
 	return 0;
 }
